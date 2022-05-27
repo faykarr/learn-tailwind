c2d38f5859dff1c91f094807e8a9dcbf389da866
faykarr <nasyathfaykar@gmail.com>
2022-05-27 16:23:59 +0700

    My first landing page

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px'
+    },
+    extend: {
+      colors: {
+        primary: '#0891b2',
+        secondary: '#64748b',
+        dark: '#1e293b'
+      },
+    },
+  },
+  plugins: [],
+}
